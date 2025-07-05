acdf0aa12e3da348f88afc33bd9069e4a480f125
Abdelrahman66880 <abdofathi317@gmail.com>
2025-07-06 02:11:54 +0300

    expirable product

--- main.py
+++ main.py
@@ -20,4 +20,18 @@ class Product:
     def is_expirable(self):
         return False
 
+
+class ExpirableProduct(Product):
+    def __init__(self, name, price, quantity, expiry_date):
+        super().__init__(name, price, quantity)
+        self.expiry_date = expiry_date
+    
+    def is_expirable(self):
+        return True
+    
+    def is_expired(self):
+        return self.expiry_date < date.today
+    
+        
+
         
